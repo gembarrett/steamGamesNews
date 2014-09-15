6ed15a39f80cc11cea820fa4670c760b016d1201
Gem Barrett <gem@gembarrett.com>
2014-09-15 23:31:10 +0100

    Updated UIAlertView to UIAlertController
    Apparently UIAlertView is deprecated, so updating seemed like a good
    idea (and the code is nicer)

--- SteamGamesNews/SteamUserViewController.swift
+++ SteamGamesNews/SteamUserViewController.swift
@@ -8,21 +8,19 @@
 
 import Foundation
 import UIKit
+import SystemConfiguration
 
 class SteamUserViewController: UIViewController {
 
     @IBOutlet weak var inputUsername: UITextField!
     @IBOutlet weak var submitUsername: UIButton!
+    var vanityUsername : String = ""
+
 
     @IBAction func vanityidExplanation(sender: AnyObject) {
-        var myAlertView = UIAlertView()
-        
-        myAlertView.title = "What is my vanity ID?"
-        myAlertView.message = "\r Your vanity ID is at the end of your Steam profile URL \r (e.g. http://steamcommunity.com/id/YOUR-VANITY-ID)"
-        myAlertView.addButtonWithTitle("Dismiss")
-        
-        myAlertView.show()
-
+        var vanityIDAlert = UIAlertController(title: "What is my vanity ID?", message: "\r Your vanity ID is at the end of your Steam profile URL \r (e.g. http://steamcommunity.com/id/YOUR-VANITY-ID)", preferredStyle: UIAlertControllerStyle.Alert)
+        vanityIDAlert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(vanityIDAlert, animated: true, completion: nil)
     }
     
 
@@ -38,12 +36,10 @@ class SteamUserViewController: UIViewController {
 
     }
     
-        var vanityUsername : String = ""
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
+    
     
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
